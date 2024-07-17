cef6e381e5ff2a3cbc33ebcaadc133577e7bba9f
Weilong Guan <weilong.guan@student.manchester.ac.uk>
2024-07-17 18:03:20 +0800

    Implement getting bill information from ChatGPT-4o, to be tested

--- backend/Split-backend/routes/model.js
+++ backend/Split-backend/routes/model.js
@@ -1,6 +1,9 @@
-require('dotenv').config()
-var express = require('express')
-var router = express.Router()
+import OpenAI from 'openai';
+
+require('dotenv').config();
+var express = require('express');
+var request = require('request');
+var router = express.Router();
 
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = process.env.MONGODB_URI
@@ -12,10 +15,54 @@ const client = new MongoClient(uri, {
   }
 });
 
+const openai = new OpenAI();
+
 /* GET the information of a bill from a model */
 router.get('/', function(req, res, next) {
   async function run() {
-    // TODO.
+    // Upload the image to PICUI.
+    const header = {
+      'Authorization': `Bearer ${process.env.PICUI_KEY}`,
+      'Accept': 'application/json',
+      'Content-Type': 'multipart/form-data'
+    };
+    const postData = {
+      'file': req.body.image,
+      'permission': 0
+    }
+    var img_url;
+    request.post({
+      url: 'https://api.picui.com/v1/upload',
+      headers: header,
+      formData: postData
+    }, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      img_url = JSON.parse(body).data.links.url;
+    });
+
+    // Get the information of the bill from ChatGPT-4o.
+    var response = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      response_format: { type: 'json_object' },
+      messages: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: 'Some text.' // To be changed to a proper prompt.
+            }, {
+              type: 'image_url',
+              image_url: img_url
+            }
+          ]
+        }
+      ]
+    });
+    res.send({'success': true, 'text': response.data.choices[0]});
   }
   run().catch(console.dir);
 });
